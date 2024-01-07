7b3d72d0b75fc4968f7d62153081e700aec13977
4nth0ny1 <acatullo4@gmail.com>
2024-01-07 00:26:21 -0600

    scaled up wizard image

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ pygame.display.set_caption('Shooter')
 
 x = 200
 y = 200
+scale = 3
 img = pygame.image.load('images/player/idle/0.png')
+img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
 rect = img.get_rect()
 rect.center = (x, y)
 
